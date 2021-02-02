f7adc79b297c14aa081f6268954cc6ea1eb11d72
MrPork1 <kelly.sean98@gmail.com>
2021-02-02 00:50:07 -0800

    Changed files

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-function addMovieEvent()
+let moviesArray = [];
+
+function createMovieObjectAndValidate()
 {
     let inputField = document.forms["addMovieForm"]["movieTitle"].value;
     let ratingField = document.forms["addMovieForm"]["movieRating"].value;
 
-    if (inputField == "" || ratingField > 5 || ratingField < 0)
+    let newMovie = new Movie(inputField, ratingField);
+    if (newMovie.validate() == true)
+    {
+        console.log("adding movie to array!");
+        moviesArray.push(newMovie);
+    }
+    else
     {
         alert("Data was not entered correctly. Try again.");
+
     }
-    
+
     let movieTitleText = document.getElementById("movieTitle").value = '';
     let ratingMovieText = document.getElementById("movieRating").value = '';
+}
+
+function showMovieList()
+{
+    let myUI = document.getElementById("moviesListHere");
+    myUI.innerHTML = "";
+
+    let ul = document.createElement('ul');
+    document.getElementById('moviesListHere').appendChild(ul);
+    for(let i = 0; i < moviesArray.length; i++)
+    {
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        console.log(moviesArray[i].returnStr());
+        li.innerHTML = moviesArray[i].returnStr();
+    };
 }
